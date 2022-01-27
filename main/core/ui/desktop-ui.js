a4cc49f8f75425693adb77d1b9d22197a8ff65b6
Jacó Martins <58242482+JacoMartins@users.noreply.github.com>
2022-01-27 20:43:14 -0300

    Add files via upload

--- main/core/ui/desktop-ui.js
+++ main/core/ui/desktop-ui.js
@@ -1,49 +1,3 @@
-window.addEventListener("load", function(){
-    dragElement(document.getElementById("developer-window-background"));
-    
-    function dragElement(elmnt) {
-      var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-      if (document.getElementById("developer-window-header")) {
-        /* if present, the header is where you move the DIV from:*/
-        document.getElementById("developer-window-header").onmousedown = dragMouseDown;
-      } else {
-        /* otherwise, move the DIV from anywhere inside the DIV:*/
-        elmnt.onmousedown = dragMouseDown;
-      }
-    
-      function dragMouseDown(e) {
-        e = e || window.event;
-        e.preventDefault();
-        // get the mouse cursor position at startup:
-        pos3 = e.clientX;
-        pos4 = e.clientY;
-        document.onmouseup = closeDragElement;
-        // call a function whenever the cursor moves:
-        document.onmousemove = elementDrag;
-      }
-    
-      function elementDrag(e) {
-        e = e || window.event;
-        e.preventDefault();
-        // calculate the new cursor position:
-        pos1 = pos3 - e.clientX;
-        pos2 = pos4 - e.clientY;
-        pos3 = e.clientX;
-        pos4 = e.clientY;
-        // set the element's new position:
-        elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-        elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
-      }
-    
-      function closeDragElement() {
-        /* stop moving when mouse button is released:*/
-        document.onmouseup = null;
-        document.onmousemove = null;
-      }
-    }
-    
-    });
-
 function menutrigger(menumain = document.getElementById('desktop-menu-main'), menusettings = document.getElementById('desktop-menu-settings')){
     if(menumain.style.display === "none"){
         menumain.style.display = "block";
@@ -67,9 +21,11 @@ function settingstrigger(menusettings = document.getElementById('desktop-menu-se
 function opensettingsapp(app = document.getElementById('settings-window')){
     app.style.display = "block";
     menumain = document.getElementById('desktop-menu-main')
+    menusettings = document.getElementById('desktop-menu-settings')
 
     if(menumain.style.display = "block"){
         menumain.style.display = "none";
+        menusettings.style.display = "none";
     }
 }
 
@@ -80,3 +36,49 @@ function closesettingsapp(app = document.getElementById('settings-window')){
 function igorcareca(desktop = document.getElementById("owner-user")){
     desktop.style.backgroundImage = "url('../../res/images/background/igor careca.png')";
 }
+
+window.addEventListener("load", function(){
+  dragElement(document.getElementById("settings-window"));
+  
+  function dragElement(elmnt) {
+    var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+    if (document.getElementById("settings-window-header")) {
+      /* if present, the header is where you move the DIV from:*/
+      document.getElementById("settings-window-header").onmousedown = dragMouseDown;
+    } else {
+      /* otherwise, move the DIV from anywhere inside the DIV:*/
+      elmnt.onmousedown = dragMouseDown;
+    }
+  
+    function dragMouseDown(e) {
+      e = e || window.event;
+      e.preventDefault();
+      // get the mouse cursor position at startup:
+      pos3 = e.clientX;
+      pos4 = e.clientY;
+      document.onmouseup = closeDragElement;
+      // call a function whenever the cursor moves:
+      document.onmousemove = elementDrag;
+    }
+  
+    function elementDrag(e) {
+      e = e || window.event;
+      e.preventDefault();
+      // calculate the new cursor position:
+      pos1 = pos3 - e.clientX;
+      pos2 = pos4 - e.clientY;
+      pos3 = e.clientX;
+      pos4 = e.clientY;
+      // set the element's new position:
+      elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+      elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+    }
+  
+    function closeDragElement() {
+      /* stop moving when mouse button is released:*/
+      document.onmouseup = null;
+      document.onmousemove = null;
+    }
+  }
+  
+  });
