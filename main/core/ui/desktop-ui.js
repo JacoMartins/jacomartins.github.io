6076499c05b1d16ba43bbf29726cf3d18cd31ad5
Jacó Martins <58242482+JacoMartins@users.noreply.github.com>
2022-03-11 20:43:39 -0300

    Add files via upload

--- main/core/ui/desktop-ui.js
+++ main/core/ui/desktop-ui.js
@@ -12,6 +12,12 @@ if (localStorage.getItem('username') == undefined){
   localStorage.setItem('username', 'System');
 }
 
+
+if (localStorage.getItem('notepad') == undefined){
+  localStorage.setItem('notepad', '');
+}
+
+
 if (localStorage.getItem('userpw') == undefined) {
   localStorage.setItem('userpw', '')
 }
@@ -2194,22 +2200,286 @@ function changeusersettingsapp(unameinput = document.getElementById('settings-wi
     localStorage.setItem('userpw', userpwinput.value);
 }
 
-function settingscategoriesusers(name){
-  let allcategories = ['users', 'appearance', 'apps', 'language', 'info'];
-  var category = 'settings-window-categories-';
+// Notepad
 
-  if(name = 'users'){
-    for(n = 1; n < allcategories.length; n++){
-      document.getElementById(category + allcategories[1]).style.display = 'none';
-    }
-  }
+let notepadwindowopen = false;
 
-  if(name = 'appearance'){
-    for(n = 2; n < allcategories.length; n++){
-      document.getElementById(category + allcategories[2]).style.display = 'none';
-    }
-  }
-
-  document.getElementById(category + name).style.display = 'block';
+function opennotepadapp(app = document.getElementById('notepad-window')){
+  notepadwindowopen = true;
+  var taskbarminimizedapps = document.getElementById('desktop-taskbar-minimized-apps');
+  var miniapp = document.createElement('button');
+  var miniappclass = document.createAttribute('class');
+  var miniappid = document.createAttribute('id');
+  var miniapponclick = document.createAttribute('onclick');
+  var miniappclassname = miniappclass.value = 'desktop-taskbar-app-button';
+  var miniappidname = miniappid.value = 'desktop-taskbar-notepad-app-button';
+  var miniapponclickfunction = miniapponclick.value = 'minimizenotepadapp()';
+  var resizebutton = document.getElementById("notepad-window-resize-button");
+  var minimizebutton = document.getElementById("notepad-window-minimize-button");
   
+  taskbarminimizedapps.appendChild(miniapp);
+  miniapp.miniappclassname;
+  miniapp.setAttributeNode(miniappclass);
+  miniapp.miniappidname;
+  miniapp.setAttributeNode(miniappid);
+  miniapp.miniapponclickfunction;
+  miniapp.setAttributeNode(miniapponclick);
+  
+  app.style.display = "block";
+  app.style.animation = 'openwindow 0.25s';
+  z++;
+  app.style.zIndex = z;
+  document.getElementById('desktop-taskbar').style.zIndex = z + 999;
+  document.getElementById('desktop-menu-main').style.zIndex = z + 998;
+  document.getElementById('desktop-menu-settings').style.zIndex = z + 998;
+  miniapp.style.display = "inline-block";
+  menumain = document.getElementById('desktop-menu-main')
+  menusettings = document.getElementById('desktop-menu-settings')
+
+  if(menumain.style.display = "block"){
+      menumain.style.display = "none";
+      menusettings.style.display = "none";
+  }
+
+  app.style.width = '1000px';
+	app.style.height = '500px';
+  app.style.top = '120px';
+	app.style.left = '120px';
+  app.style.resize = "both";
+  app.style.borderRadius = "4px";
+  resizebutton.style.display = 'block';
+  minimizebutton.style.left = '51px';
+
+  if (window.innerHeight > window.innerWidth){
+    restoreheight = app.style.height;
+    restorewidth = app.style.width; 
+    restoretop = app.style.top;
+    restoreleft = app.style.left;
+		app.style.width = '100%';
+		app.style.height = 'calc(100% - 50px)';
+		app.style.top = "0px";
+		app.style.left = "0px";
+    app.style.resize = "none";
+    app.style.borderRadius = "0px";
+    app.style.transition = "0.15s";
+    resizebutton.style.display = 'none';
+    minimizebutton.style.left = '31px';
+    setTimeout(function() {
+			  app.style.transition = "none";
+		}, 150)
+  }
+
+  document.getElementById('notepad-window-content-text-editor').value = localStorage.getItem('notepad');
+
+  restoreheight = app.style.height;
+  restorewidth = app.style.width;
+  restoretop = app.style.top;
+  restoreleft = app.style.left;
 }
+
+function notepadappfocus(app = document.getElementById('notepad-window')){
+  z++;
+  app.style.zIndex = z;
+  document.getElementById('desktop-taskbar').style.zIndex = z + 999;
+  document.getElementById('desktop-menu-main').style.zIndex = z + 998;
+  document.getElementById('desktop-menu-settings').style.zIndex = z + 998;
+}
+
+function closenotepadapp(app = document.getElementById('notepad-window'), miniapp = document.getElementById('desktop-taskbar-notepad-app-button')){
+  var taskbarminimizedapps = document.getElementById('desktop-taskbar-minimized-apps');
+  
+  app.style.animation = 'closewindow 0.25s';
+  app.style.animationDuration = '0.25s';
+  setTimeout(() => {
+    app.style.display = "none";
+    miniapp.parentNode.removeChild(miniapp);
+    localStorage.setItem('notepad', document.getElementById('notepad-window-content-text-editor').value);
+    notepadwindowopen = false;
+  }, 250);
+}
+
+function resizenotepadapp(app = document.getElementById("notepad-window")){
+  if (app.style.width === '100%') {
+		app.style.width = restorewidth;
+		app.style.height = restoreheight;
+    app.style.left = restoreleft;
+    app.style.top = restoretop;
+    app.style.resize = "both";
+    app.style.borderRadius = "4px";
+    app.style.animation = 'maximizewindow 0.25s';
+    app.style.transition = "0.15s";
+    setTimeout(function() {
+			  app.style.transition = "none";
+		}, 150)
+	} else {
+    restoreheight = app.style.height;
+    restorewidth = app.style.width; 
+    restoretop = app.style.top;
+    restoreleft = app.style.left;
+		app.style.width = '100%';
+		app.style.height = 'calc(100% - 40px)';
+		app.style.top = "40px";
+		app.style.left = "0px";
+    app.style.resize = "none";
+    app.style.borderRadius = "0px";
+    app.style.transition = "0.15s";
+    setTimeout(function() {
+			  app.style.transition = "none";
+		}, 150)
+	}
+}
+
+function minimizenotepadapp(app = document.getElementById("notepad-window"), miniapp = document.getElementById('desktop-taskbar-notepad-app-button')) {
+  if (notepadwindowopen = true) {
+    if(app.style.display === 'block'){
+      app.style.animation = 'minimizewindow 0.25s';
+      app.style.animationDuration = '0.25s';
+      setTimeout(() => {
+        app.style.display = 'none';
+      }, 250);
+    }
+    else {
+      app.style.display = 'block';
+      app.style.animation = 'callwindow 0.25s';
+      notepadappfocus()
+    }
+  }
+  else {
+    opennotepadapp()
+  }
+}
+
+function sidenotepadapp(app = document.getElementById("notepad-window")){
+  var appleft = parseInt(app.style.left, 10);
+  var appwidth = parseInt(app.style.width, 10);
+  
+  if (appleft < 0) {
+    app.style.left = '0px';
+    app.style.top = '40px';
+    app.style.height = 'calc(100% - 40px)';
+    app.style.width = '50%';
+    app.style.resize = "none";
+    app.style.borderRadius = "0px";
+    app.style.transition = "0.15s";
+    setTimeout(function() {
+			  app.style.transition = "none";
+		}, 150)
+  }
+
+  if (appleft > (window.innerWidth - appwidth)) {
+    app.style.left = '50%';
+    app.style.top = '40px';
+    app.style.height = 'calc(100% - 40px)';
+    app.style.width = '50%';
+    app.style.resize = "none";
+    app.style.borderRadius = "0px";
+    app.style.transition = "0.15s";
+    setTimeout(function() {
+			  app.style.transition = "none";
+		}, 150)
+  }
+}
+
+function snapminnotepadapp(app = document.getElementById("notepad-window")){
+  var appwidth = parseInt(restorewidth, 10);
+  var posleft = window.event.clientX - (appwidth * 0.5);
+  var posleftcss = posleft + 'px';
+  var postop = window.event.clientY + 'px';
+
+  if (app.style.height === 'calc(100% - 40px)') {
+		app.style.width = restorewidth;
+		app.style.height = restoreheight;
+    app.style.left = posleftcss;
+    app.style.top = '40px';
+    app.style.resize = "both";
+    app.style.borderRadius = "4px";
+	}
+}
+
+function snapnotepadapp(app = document.getElementById("notepad-window")){
+  var clienttop = window.event.clientY;
+  var clientleft = window.event.clientX;
+
+  if (clienttop <= 0) {
+    if (app.style.width === 'calc(100% - 40px)') {
+      app.style.width = restorewidth;
+      app.style.height = restoreheight;
+      app.style.resize = "both";
+      app.style.borderRadius = "4px";
+    } else {
+      app.style.width = '100%';
+      app.style.height = 'calc(100% - 40px)';
+      app.style.top = "40px";
+      app.style.left = "0px";
+      app.style.resize = "none";
+      app.style.borderRadius = "0px";
+      app.style.transition = "0.15s";
+      setTimeout(function() {
+			  app.style.transition = "none";
+		}, 150)
+    }
+  }
+
+  if (clientleft <= 0) {
+    sidenotepadapp()
+    // document.getElementById("notepad-window-header-title").innerHTML = 'Detected';
+  }
+
+  if(clientleft >= (window.innerWidth - 1)){
+    sidenotepadapp()
+  }
+}
+
+function saverestorepos(app = document.getElementById('notepad-window')){
+  restoreheight = app.style.height;
+  restorewidth = app.style.width; 
+  restoretop = app.style.top;
+  restoreleft = app.style.left;
+}
+
+window.addEventListener("load", function(){
+dragElement(document.getElementById("notepad-window"));
+
+function dragElement(elmnt) {
+  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+  if (document.getElementById("notepad-window-header")) {
+    /* if present, the header is where you move the DIV from:*/
+    document.getElementById("notepad-window-header").onmousedown = dragMouseDown;
+  } else {
+    /* otherwise, move the DIV from anywhere inside the DIV:*/
+    elmnt.onmousedown = dragMouseDown;
+  }
+
+  function dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // get the mouse cursor position at startup:
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    // call a function whenever the cursor moves:
+    document.onmousemove = elementDrag;
+  }
+
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // calculate the new cursor position:
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    // set the element's new position:
+    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+    snapminnotepadapp();
+  }
+
+  function closeDragElement() {
+    document.onmouseup = null;
+    document.onmousemove = null;
+    snapnotepadapp();
+  }
+}
+
+});
